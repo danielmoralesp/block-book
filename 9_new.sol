2befe7c71ad1ecbaf00373565d049316c5846293
danielmoralesp <daniel_moralesp@hotmail.com>
2018-01-23 16:22:59 -0500

    new constructor

---
+++ 9_new.sol
@@ -0,0 +1,16 @@
+pragma Solidity ^0.4.2;
+
+contract sample1 {
+  int a;
+
+  function assign(int b){
+    a = b
+  }
+}
+
+contract sample2 {
+  function sample2(){
+    sample1 s = new sample1();
+    s.assign(12);
+  }
+}
