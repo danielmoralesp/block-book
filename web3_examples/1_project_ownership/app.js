927cbb9c84f206f038320943b0b1dcd70eb55fa1
danielmoralesp <daniel_moralesp@hotmail.com>
2018-01-29 11:14:18 -0500

    Serve static files

--- web3_examples/1_project_ownership/app.js
+++ web3_examples/1_project_ownership/app.js
@@ -2,4 +2,11 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
+
 server.listen(8080);
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/public/html/index.html");
+})
